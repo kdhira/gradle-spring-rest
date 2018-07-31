f14f8f7032c2c3ceacc5cf96819ed455fa9ac28f
Kevin Hira <kevinhi@orionhealth.com>
2018-07-31 20:44:58 +1200

    Add src code

---
+++ src/main/java/com/kevinhira/Greeting.java
@@ -0,0 +1,19 @@
+package com.kevinhira;
+
+public class Greeting {
+    private final long _id;
+    private final String _content;
+
+    public Greeting(long id, String content) {
+        _id = id;
+        _content = content;
+    }
+
+    public long getId() {
+        return _id;
+    }
+
+    public String getContent() {
+        return _content;
+    }
+}
